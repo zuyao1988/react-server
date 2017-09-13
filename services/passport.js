c338d4705ceec919a8280934b048d640137ea9b0
zuyao <zuyao1988@gmail.com>
2017-09-13 18:10:31 +0800

    trusted proxy in passport

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
+		callbackURL: '/auth/google/callback',
+		proxy: true
 	}, (accessToken, refreshToken, profile, done) => {
 		User.scan({ googleId: profile.id }, function(err, existingUser) {
 			if (existingUser.count > 0) {
